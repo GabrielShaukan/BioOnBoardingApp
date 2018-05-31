53a0100828c347b03efc37856a2d47c4ba00e460
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-05-31 12:36:30 +0200

    Added SlideAdapter Class

--- app/src/main/java/com/example/gabriel/bioonboardingapp/SliderAdapter.java
+++ app/src/main/java/com/example/gabriel/bioonboardingapp/SliderAdapter.java
@@ -1,4 +1,90 @@
 package com.example.gabriel.bioonboardingapp;
 
-public class SliderAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context
+    }
+
+    //Arrays
+    public  int[] slide_Img = {
+            R.drawable.android,
+            R.drawable.c_logo,
+            R.drawable.circuit_board,
+            R.drawable.coding,
+            R.drawable.folder,
+            R.drawable.karaoke,
+            R.drawable.playstation
+    };
+
+    public String[] slide_Headings = {
+            "Android",
+            "C++",
+            "Electrical Engineering",
+            "Code",
+            "Frontend Web Development",
+            "Sing",
+            "Games"
+    };
+
+    public String[] slide_Desc = {
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi"
+    };
+
+    @Override
+    public int getCount() {
+        return slide_Headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (RelativeLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        LayoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImg = (ImageView) view.findViewById(R.id.slideImg);
+        TextView slideHeading = (TextView) view.findViewById(R.id.slideHeading);
+        TextView slideDesc = (TextView) view.findViewById(R.id.slideDesc);
+
+        slideImg.setImageResource(slide_Img[position]);
+        slideHeading.setText(slide_Headings[position]);
+        slideDesc.setText(slide_Desc[position]);
+
+        container.addView(view);
+
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+
+        container.removeView((RelativeLayout)object);
+
+    }
 }
